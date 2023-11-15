9b6dcfc7ea48367b14a0d7f17e39275099579388
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-15 12:08:36 +0200

    Auto Implemented Properties

---
+++ 05. Auto Implemented Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.Auto_Implemented_Properties
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+
+            Console.WriteLine(employee1.Id);
+            Console.WriteLine(employee1.Name);
+
+            employee1.Id = 1;
+            employee1.Name = "Mohamed Badwy";
+
+            Console.WriteLine(employee1.Id);
+            Console.WriteLine(employee1.Name);
+        }
+    }
+}
