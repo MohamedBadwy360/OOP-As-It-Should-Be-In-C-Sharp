a3b635f67a7064aefefbb9b0c56bde0759a190d2
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-15 13:41:30 +0200

    Parameter-less Constructor

---
+++ 06. Parameterless Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace _06.Parameterless_Constructor
+{
+    class Person
+    {
+        public int Id {set; get;}
+        public string Name { set; get;}
+        public byte Age { set; get;}
+        
+        // Parameterless Constructor
+        public Person()
+        {
+            Id = -1;
+            Name = "Empyt";
+            Age = 0;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            Console.WriteLine(person1.Id);
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person1.Age);
+        }
+    }
+}
