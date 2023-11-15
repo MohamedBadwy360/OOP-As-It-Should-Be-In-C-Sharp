2b3b56c4e51257bf2a505cda1b7e972f7c784cf3
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-15 10:15:27 +0200

    Class & Object

---
+++ 01. Class & Object/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _01.Class___Object
+{
+    class clsPerson
+    {
+        public string FirstName;
+        public string LastName;
+
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsPerson person1 = new clsPerson();
+            Console.WriteLine(person1.FullName());
+            Console.WriteLine(person1.FullName());
+
+            person1.FirstName = "Mohamed";
+            person1.LastName = "Badwy";
+            Console.WriteLine(person1.FullName());
+
+        }
+    }
+}
