614560e3680cdb1247070ba6da000ec4816f982f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-15 12:02:53 +0200

    Property Get and Set

---
+++ 04. Properities Set and Get/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace _04.Properities_Set_and_Get
+{
+    class clsEmployee
+    {
+        private int _ID;
+        private string _Name = string.Empty;
+
+        public int ID
+        {
+            set
+            {
+                _ID = value;
+            }
+
+            get
+            {
+                return _ID;
+            }
+        }
+
+        public string Name
+        {
+            set
+            {
+                _Name = value;
+            }
+
+            get
+            {
+                return _Name;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmployee employee1 = new clsEmployee();
+
+            Console.WriteLine(employee1.ID);
+            Console.WriteLine(employee1.Name);
+
+            employee1.ID = 1;
+            employee1.Name = "Mohamed Badwy";
+
+            Console.WriteLine(employee1.ID);
+            Console.WriteLine(employee1.Name);
+        }
+    }
+}
