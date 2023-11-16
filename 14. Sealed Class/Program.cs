d09578c20b58a1a7739e4347d31c967b145306ed
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 17:12:59 +0200

    Sealed Class

---
+++ 14. Sealed Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace _14.Sealed_Class
+{
+    sealed class clsA
+    {
+
+    }
+
+    // Cant't inherit from class clsA
+    class clsB : clsA
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
