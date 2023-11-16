3b333d93d0cf4f3fb8a54daaafb79b0855015522
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 11:42:21 +0200

    Method Overriding

---
+++ 09. Method Overriding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _09.Method_Overriding
+{
+    internal class Program
+    {
+        class clsA
+        {
+            public virtual void Print()
+            {
+                Console.WriteLine("From Class A");
+            }
+        }
+
+        class clsB : clsA
+        {
+            public override void Print()
+            {
+                //base.Print();
+                Console.WriteLine("From class B");
+            }
+        }
+        static void Main(string[] args)
+        {
+            clsB objB = new clsB();
+            objB.Print();
+        }
+    }
+}
