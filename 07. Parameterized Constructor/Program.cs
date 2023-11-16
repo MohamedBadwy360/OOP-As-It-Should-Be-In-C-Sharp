e71c1c98cdbc5174c7ea6c5fa4a9d0e74485848f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 08:06:17 +0200

    Parameterized Constructors

---
+++ 07. Parameterized Constructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace _07.Parameterized_Constructor
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte Age { get; set; }
+
+        public Employee(int Id, string Name, byte Age)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public Employee()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(1, "Mohamed Badwy", 23);
+
+            Console.WriteLine($"ID = {employee.Id}");
+            Console.WriteLine($"Name = {employee.Name}");
+            Console.WriteLine($"Age = {employee.Age}");
+        }
+    }
+}
