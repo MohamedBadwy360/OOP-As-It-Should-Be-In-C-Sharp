3fbbf2c41a28bded08eac6e46b192989fafaf631
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 16:33:05 +0200

    Interfaces

---
+++ 12. Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _12.Interfaces
+{
+    internal class Program
+    {
+        public interface IPerson
+        {
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+            void Introuduce();
+
+            void Print();
+        }
+
+        public abstract class Person : IPerson
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public abstract void Introuduce();
+
+            public abstract void Print();
+
+            public void GoodBye()
+            {
+                Console.WriteLine("GoodBye");
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
