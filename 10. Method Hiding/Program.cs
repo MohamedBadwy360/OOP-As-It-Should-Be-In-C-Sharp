ebb7eb9ac7f0261aa32c46ba8a8cae69c6a91c21
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 13:57:58 +0200

    Method Shadowing

---
+++ 10. Method Hiding/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class BaseClass
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("Base Class Print Function Implelmentation");
+    }
+
+    public virtual void OtherPrint()
+    {
+        Console.WriteLine("Base Class Other Print Function Implelmentation");
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public override void Print()
+    {
+        Console.WriteLine("Derived Class Print Function Implelmentation");
+    }
+
+    // Hiding or Shadowing
+    public new void OtherPrint()
+    {
+        Console.WriteLine("Derived Class Other Print Function Implelmentation");
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Base Class");
+        BaseClass objBase = new BaseClass();
+        objBase.Print();
+        objBase.OtherPrint();
+
+
+        Console.WriteLine("\nDerived Class");
+        DerivedClass objDerived = new DerivedClass();
+        objDerived.Print();
+        objDerived.OtherPrint();
+
+        Console.WriteLine("\nAfter Casting");
+        BaseClass objBase2 = new DerivedClass();
+        objBase2.Print();
+        objBase2.OtherPrint();
+    }
+}
+
