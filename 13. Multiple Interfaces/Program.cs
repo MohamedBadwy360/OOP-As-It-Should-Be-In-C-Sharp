11cacfa4335d0c421a656a35da9e8bfe5306b876
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 17:12:47 +0200

    Multiple Interfaces

---
+++ 13. Multiple Interfaces/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _12.Interfaces
+{
+    internal class Program
+    {
+        public interface IPerson
+        {
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+            void Introuduce();
+
+            void Print();
+        }
+
+        public interface ICommunicate
+        {
+            void SendEmail();
+
+            void SendFax();
+
+            void SendSMS();
+        }
+
+        public abstract class Person : IPerson, ICommunicate
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public abstract void Introuduce();
+
+            public abstract void Print();
+
+            public void GoodBye()
+            {
+                Console.WriteLine("GoodBye");
+            }
+
+            public abstract void SendEmail();
+
+            public abstract void SendFax();
+
+            public abstract void SendSMS();
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
