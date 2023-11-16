236ffab6095330014041889e4abe62dde7c4e0dc
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-16 14:29:20 +0200

    Abstract Class & Methods

---
+++ 11. Abstract Class/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _11.Abstract_Class
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+
+        public abstract void Introduce();
+
+        public void SayGoodBye()
+        {
+            Console.WriteLine("GoodBye");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"My name is {Name} and My Id is {Id}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cant't create amn object from abstract class 
+            //Person person = new Person();
+
+            Employee employee = new Employee();
+
+            employee.Name = "Mohamed Badwy";
+            employee.Id = 1;
+
+            employee.SayGoodBye();
+            employee.Introduce();
+        }
+    }
+}
